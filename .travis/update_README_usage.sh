f64533b0c0f020ddfe9f1b484d57ecfbc47de6eb
Jon Clucas <jon@vasegurt.com>
2020-04-24 18:19:34 -0400

    :construction_worker: Wait for move before compare

--- .travis/update_README_usage.sh
+++ .travis/update_README_usage.sh
@@ -5,7 +5,8 @@ printf ".. code-block:: shell\n\n    cpac --help\n"; \
 cpac --help | sed 's/^/    /'; \
 tail --lines=+$(read -d : <<< $(less README.rst | grep ".. END USAGE" -n); expr $REPLY - 1) README.rst\
 ) > tempREADME
-mv tempREADME README.rst
+mv tempREADME README.rst &
+wait %1
 
 if [ "$(cat README.rst)" = "$original_README" ]; then
     original_ORIGIN=$(git remote get-url origin)
