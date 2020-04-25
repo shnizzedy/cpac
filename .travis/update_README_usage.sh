f933cd5cc33b7078e6231e07d35ee7d76e5bb912
Jon Clucas <jon@vasegurt.com>
2020-04-24 20:12:35 -0400

    :green_heart: TRAVIS_BRANCH

--- .travis/update_README_usage.sh
+++ .travis/update_README_usage.sh
@@ -9,10 +9,8 @@ mv tempREADME README.rst &
 wait %1
 
 if [ "$(cat README.rst)" = "$original_README" ]; then
-    original_ORIGIN=$(git remote get-url origin)
-    git remote remove origin
-    git remote add origin $"https://${GITHUB_USERNAME}:${GITHUB_PUSH_TOKEN}@github.com/"${original_ORIGIN:19}
+    git remote set-url origin $"https://${GITHUB_USERNAME}:${GITHUB_PUSH_TOKEN}@github.com/"${$(git remote get-url origin):19}
     git add README.rst
     git commit -m ":books: Update usage from helpstring"
-    git push origin $(git rev-parse --abbrev-ref HEAD)
+    git push origin $TRAVIS_BRANCH
 fi
