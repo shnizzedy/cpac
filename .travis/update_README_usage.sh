ddaa83fd74827ebe258172117caad181d2b7a03e
Jon Clucas <jon@vasegurt.com>
2020-04-27 09:36:01 -0400

    :construction_worker: Skip Travis on README updates

--- .travis/update_README_usage.sh
+++ .travis/update_README_usage.sh
@@ -1,17 +1,15 @@
-original_README=$(cat README.rst)
-
 (head -n $(read -d : <<< $(less README.rst | grep ".. BEGIN USAGE" -n); expr $REPLY + 1) README.rst; \
 printf ".. code-block:: shell\n\n    cpac --help\n"; \
 cpac --help | sed 's/^/    /'; \
 tail --lines=+$(read -d : <<< $(less README.rst | grep ".. END USAGE" -n); expr $REPLY - 1) README.rst\
-) > tempREADME
+) > tempREADME &&
 mv tempREADME README.rst &
 wait %1
 
-if [ "$(cat README.rst)" = "$original_README" ]; then
+if [[ $(git diff --numstat README.rst) ]]; then
     OLD_ORIGIN=$(git remote get-url origin)
     git remote set-url origin $"https://${GITHUB_USERNAME}:${GITHUB_PUSH_TOKEN}@github.com/"${OLD_ORIGIN:19}
     git add README.rst
-    git commit -m ":books: Update usage from helpstring"
+    git commit -m ":books: Update usage from helpstring" -m "[skip travis]"
     git push origin HEAD:$TRAVIS_BRANCH
 fi
